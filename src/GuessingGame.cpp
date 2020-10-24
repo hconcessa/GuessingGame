e96e24f7ec9a7f8cacb790abc712e80aa7d25f0f
Higor Maia <higor.maia@gertec.com.br>
2020-10-24 00:49:12 -0300

    First files

---
+++ src/GuessingGame.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GuessingGame.cpp
+// Author      : Higor Maia Concessa
+// Version     : 1.0.0
+// License     : GNU General Public License v3.0
+// Description : A simple guessing game in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World yes!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
